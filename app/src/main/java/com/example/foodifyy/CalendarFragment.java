45d36c45598a906f5592e963525447989fe50f56
Jay <jrsaranza92@gmail.com>
2023-10-23 23:04:19 +0800

    Minor Changes

--- app/src/main/java/com/example/foodifyy/CalendarFragment.java
+++ app/src/main/java/com/example/foodifyy/CalendarFragment.java
@@ -59,6 +59,8 @@ public class CalendarFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_calendar, container, false);
+        View v = inflater.inflate(R.layout.fragment_calendar, container, false);
+
+        return v;
     }
 }
