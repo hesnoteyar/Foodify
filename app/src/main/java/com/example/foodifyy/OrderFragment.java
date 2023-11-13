907d35a9af6c0c65bf7cc24aace5383b65f6f2e2
Jay <jrsaranza92@gmail.com>
2023-11-13 22:19:02 +0800

    Minor Changes

--- app/src/main/java/com/example/foodifyy/OrderFragment.java
+++ app/src/main/java/com/example/foodifyy/OrderFragment.java
@@ -7,6 +7,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +29,9 @@ public class OrderFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    TextView greeting;
+    FirebaseAuth auth;
+    DatabaseReference databaseReference;
 
     public OrderFragment() {
         // Required empty public constructor
@@ -59,6 +68,26 @@ public class OrderFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_order, container, false);
+        View v = inflater.inflate(R.layout.fragment_order, container, false);
+
+        greeting = v.findViewById(R.id.greetings1);
+
+        auth = FirebaseAuth.getInstance();
+        FirebaseUser currentUser = auth.getCurrentUser();
+        databaseReference = FirebaseDatabase.getInstance().getReference("users").child(currentUser.getUid());
+
+        displayGreetings();
+
+        return v;
+    }
+
+    private void displayGreetings() {
+        databaseReference.get().addOnSuccessListener(documentSnapshot -> {
+           if (documentSnapshot.exists()) {
+               String greet = documentSnapshot.child("username").getValue(String.class);
+
+               greeting.setText("Hi, " + greet + "!");
+           }
+        });
     }
 }
